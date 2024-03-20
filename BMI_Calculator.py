a7fd48eb754bd5a58d6592002e1ad97b9f59b8d6
AYUSHDEVANI <ayushdevani0018@gmail.com>
2024-03-20 22:28:32 +0530

    Make a function for interpret bmi into different types.

--- BMI_Calculator.py
+++ BMI_Calculator.py
@@ -14,3 +14,17 @@ def get_user_input(prompt):
 def claculate_bmi(weight, height):
     bmi = weight / (height ** 2)
     return bmi
+
+def interpret_bmi(bmi):
+    if bmi < 18.5:
+        return "Underweight"
+    
+    elif 18.5 <= bmi < 25:
+        return "Normal Weight"
+
+    elif 25 <= bmi < 30:
+        return "Overweight"
+
+    else:
+        return "Obese"
+
