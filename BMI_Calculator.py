9cdf17208fbb2a1e811130c3853cb9aa8b73b569
AYUSHDEVANI <ayushdevani0018@gmail.com>
2024-03-20 22:24:26 +0530

    Start a making the BMI Calculator and create the first function for the user input.

--- BMI_Calculator.py
+++ BMI_Calculator.py
@@ -1,1 +1,13 @@
 
+
+def get_user_input(prompt):
+    while True:
+        try:
+            user_input = float(input(prompt))
+            if user_input <= 0:
+                print("Please enter a valid positive number.")
+            else:
+                return user_input
+        except ValueError:
+            print("Invalid input. Please enter a valid number.")
+
